402940b13ddfc010b9ad3b02e2417697f4259963
MamtaGarg <mamta.garg915@gmail.com>
2020-06-15 17:29:02 +0530

    Create modules and code refactoring with new Twitter Producer.

--- kafka-basics/src/main/java/com/kafka/tutorial/consumer/ConsumerDemo.java
+++ kafka-basics/src/main/java/com/kafka/tutorial/consumer/ConsumerDemo.java
@@ -5,13 +5,12 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Properties;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Properties;
 
 public class ConsumerDemo {
 
